cba397891650b00ffb07df0dd858e8d8c812c755
yshdev27 <gyash21@gmail.com>
2026-04-12 18:37:13 +0530

    add api type and types

--- src/api/pets.ts
+++ src/api/pets.ts
@@ -1,4 +1,6 @@
-export const fetchPets = async () => {
+import type { Pet } from "../types/pet";
+
+export const fetchPets = async (): Promise<Pet[]> => {
   const res = await fetch("/pets.json");
 
   if (!res.ok) throw new Error("Failed to fetch pets");
---
+++ src/types/pet.ts
@@ -0,0 +1,8 @@
+export interface Pet {
+  id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  createdAt: string;
+  size?: number;
+}
